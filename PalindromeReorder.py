37581f8d89ffca651b364a537f088e5b4ce21417
Rodriasd <veronrodrigo98@gmail.com>
2024-04-03 20:55:47 -0300

    se creo la funcion para formar un palindromo

--- PalindromeReorder.py
+++ PalindromeReorder.py
@@ -1,28 +1,36 @@
 def palindrome_reorder(palabra):
-    lista_letras = []
-    k = 0
-    for i in palabra :  
-        lista_letras.append(palabra[k])
-        k = k + 1
-    k=0
+    frecuencia_letra = {}
+    impares = []
+    mitad_izquierda = ''
+    mitad_derecha = ''
 
-    # for i in lista_letras:
-        
-    #     if lista_letras[k] == lista_letras[k+1]:
+    # Contamos con qué frecuencia se repite cada letra
+    for i in palabra:
+        if i in frecuencia_letra:
+            frecuencia_letra[i] += 1
+        else:
+            frecuencia_letra[i] = 1
 
-    #         print('a')
-    #     k=k+1
+    # Guardamos en una lista las letras que se repiten con frecuencia impar
+    for clave, valor in frecuencia_letra.items():
+        if valor % 2 != 0:
+            impares.append(clave)
 
-    pasar_string = ''.join(lista_letras)
+    # Terminamos la ejecución si hay más de 1 letra con frecuencia impar 
+    if len(impares) > 1:
+        return "NOT SOLUTION"
+
+    # Construimos la mitad izquierda concatenando las letras repetidas a la mitad de su frecuencia.
+    for clave, valor in frecuencia_letra.items():
+        mitad_izquierda += clave * (valor // 2)
+
+    # Construimos la mitad derecha invirtiendo la mitad izquierda
+    mitad_derecha = mitad_izquierda[::-1]
+
+    # Concatenamos la mitad izquierda con la letra impar (si existe), y la mitad derecha.
+    palindromo = mitad_izquierda + (impares[0] if impares else '') + mitad_derecha
+
+    return palindromo
 
 
-    if pasar_string == pasar_string[::-1]:
-      return True
-    else:
-      return False
-
-
-    return pasar_string
-
-
-print(palindrome_reorder("soo"))
+assert palindrome_reorder("aabbc") == "abcba", "Error en el caso de prueba"
